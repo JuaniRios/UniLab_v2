3929c363a2e5c0ba770ed8b9ae658aed2e9773a6
Juan Ignacio Rios <juani.rios.99@gmail.com>
2021-10-25 17:30:17 +0200

    can run, but navbar isnt fixed

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -2,24 +2,48 @@ import React, { Component } from "react";
 import {
     Route,
     NavLink,
-    HashRouter
+    HashRouter,
+
 } from "react-router-dom";
 import Home from "./Home";
-import Stuff from "./Stuff";
-import Contact from "./Contract";
-import Navbar from "./components/navbar";
+// import Navbar from "./components/navbar";
+
+
+import "./css/main_style.css";
+import "./css/top_nav.css";
+import "./css/footer.css";
+import "./css/profile_menu.css";
+import "./css/language_menu.css";
+import "./css/chatbox.css";
+import "./css/loader.css";
+
+// document.querySelector('#lang-img').src = `./img/languages/en.webp`;
+// document.querySelector('body').onload = setTimeout(function () { reveal_chats(); }, 1000);
+// // if (width > 1290) {window.onscroll = function(){scrollFunction2()};}
+//
+// function set_lang_cookie(lang) {
+//     document.cookie = `django_language=en`
+// }
+//
+// document.querySelector('.system-message').style.bottom = "5%";
+// setTimeout(
+//     function () {
+//         document.querySelector('.system-message').style.bottom = "-25%";
+//     },
+//     3000
+// )
 
 class Main extends Component {
     render() {
         return (
             <HashRouter>
                 <div>
-                    <Navbar user={get_user}/>
+                    {/*<Navbar user={get_user}/>*/}
                     <div className="content">
-                        <Route exact path="/" component={Home}/>
-                        <Route path="/stuff" component={Stuff}/>
-                        <Route path="/contact" component={Contact}/>
+                        {/*<Route exact path="/" component={Home}/>*/}
+                        Unilab Website
                     </div>
+
                 </div>
             </HashRouter>
         );
--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -5,29 +5,36 @@ import {
     HashRouter
 } from "react-router-dom";
 
+import "../scripts/main.js";
+import "../scripts/top_nav.js";
+import "../scripts/chatbox.js";
+import "../scripts/ajax.js";
+
+
+
 function Navbar(props) {
     const user = props.user
     return (
-        <!-- TOP NAVIGATION BAR -->
+        // TOP NAVIGATION BAR
         <nav className="top-nav flex-row a-i-c shadow">
 
-            <!-- LOGO -->
+            {/*LOGO*/}
             <NavLink to='/' id="logo-link" className="top-nav-item flex-row a-i-c">
-                <img id="logo-img" className="top-nav-image" src="../../static/img/unilab_logo.png" alt="UniLab Logo"
+                <img id="logo-img" className="top-nav-image" src="../img/unilab_logo.png" alt="UniLab Logo"
                      title="Home"/>
             </NavLink>
 
-            <!-- MOBILE -->
+            {/*MOBILE*/}
             <div className="menu-mobile" onClick={open_linebar}/>
 
-            <!-- MENU -->
+            {/*MENU*/}
             <div className="top-nav-container">
 
-                <!-- HOME BUTTON -->
+                {/*HOME BUTTON*/}
                 <NavLink to="/" className="top-nav-item flex-row a-i-c" onMouseOver={ () => fill_items(0) }
                    onMouseOut={ () => cancel_items(0) }>
                     <div className="top-nav-item-filler flex-row a-i-c"/>
-                    <img className="top-nav-image" src="../../static/img/top-nav/address2.png" alt="Home Icon" />
+                    <img className="top-nav-image" src="../img/top-nav/address2.png" alt="Home Icon" />
                     {/*{# Translators: Start of Navigation bar #}*/}
                     <div id="home-button" className="top-nav-item-text">
                         {/*{% translate "Home" context "this is the navbar"%}*/}
@@ -35,33 +42,33 @@ function Navbar(props) {
                     </div>
                 </NavLink>
 
-                <!-- COMMUNITY BUTTON -->
+                {/*COMMUNITY BUTTON*/}
                 <NavLink to="/community" className="top-nav-item flex-row a-i-c" onMouseOver={() => fill_items(1) }
                    onMouseOut={ () => cancel_items(1) }>
                     <div className="top-nav-item-filler flex-row a-i-c"/>
-                    <img className="top-nav-image" src="../../static/img/top-nav/community.png" alt="Community Icon" />
+                    <img className="top-nav-image" src="../img/top-nav/community.png" alt="Community Icon" />
                     <div id="community-button" className="top-nav-item-text">
                         {/*{% translate "Community"%}*/}
                         Community
                     </div>
                 </NavLink>
 
-                <!-- COMPANIES BUTTON -->
+                {/*COMPANIES BUTTON*/}
                 <NavLink to="/companies" className="top-nav-item flex-row a-i-c" onMouseOver={ () => fill_items(2) }
                    onMouseOut={ () => cancel_items(2) }>
                     <div className="top-nav-item-filler flex-row a-i-c"/>
-                    <img className="top-nav-image" src="../../static/img/top-nav/employers.png" alt="Companies Icon" />
+                    <img className="top-nav-image" src="../img/top-nav/employers.png" alt="Companies Icon" />
                     <div id="employers-button" className="top-nav-item-text">
                         {/*{% translate "Companies"%}*/}
                         Companies
                     </div>
                 </NavLink>
 
-                <!-- JOBS BUTTON -->
+                {/*JOBS BUTTON*/}
                 <NavLink to="/jobs" className="top-nav-item flex-row a-i-c" onMouseOver={ () => fill_items(3)}
                    onMouseOut={ () => cancel_items(3) }>
                     <div className="top-nav-item-filler flex-row a-i-c"/>
-                    <img className="top-nav-image" src="../../static/img/top-nav/jobs.svg" alt="Jobs Icon" />
+                    <img className="top-nav-image" src="../img/top-nav/jobs.svg" alt="Jobs Icon" />
                     <div id="jobs-button" className="top-nav-item-text">
                         {/*{% translate "Jobs"%}*/}
                         Jobs
@@ -69,18 +76,18 @@ function Navbar(props) {
 
                 </NavLink>
 
-                <!-- ABOUT BUTTON -->
+                {/*ABOUT BUTTON*/}
                 <NavLink to="/about" className="top-nav-item flex-row a-i-c" onMouseOver={ () => fill_items(4) }
                    onMouseOut={ () => cancel_items(4) }>
                     <div className="top-nav-item-filler flex-row a-i-c"/>
-                    <img className="top-nav-image" src="../../static/img/top-nav/about.png" alt="About Icon" />
+                    <img className="top-nav-image" src="../img/top-nav/about.png" alt="About Icon" />
                     <div id="about-button" className="top-nav-item-text">
                         {/*{% translate "About"%}*/}
                         About
                     </div>
                 </NavLink>
 
-                <!-- PROFILE BUTTON -->
+                {/*PROFILE BUTTON*/}
                 <a className="top-nav-item top-nav-profile flex-row a-i-c" onClick={ open_profile }>
                     <img id="profile-img" className="top-nav-image" src={user ? user['image'] : '../../static/img/top-nav/profile.png'}
                          alt="Profile Picture" title="Profile" />
@@ -91,9 +98,9 @@ function Navbar(props) {
                     </div>
                 </a>
 
-                <!-- SEARCH BUTTON -->
+                {/*SEARCH BUTTON*/}
                 <a className="top-nav-item top-nav-search flex-row a-i-c" onClick={ show_search }>
-                    <img id="search-img" className="top-nav-image" src="../../static/img/top-nav/search.png"
+                    <img id="search-img" className="top-nav-image" src="../img/top-nav/search.png"
                          alt="Magnifying Glass" title="Search" />
                     <div id="search-text" className="top-nav-item-text">
                         {/*{% translate "Search"%}*/}
@@ -101,7 +108,7 @@ function Navbar(props) {
                     </div>
                 </a>
 
-                <!-- LANGUAGE BUTTON -->
+                {/*LANGUAGE BUTTON*/}
                 <a className="top-nav-item flex-row a-i-c" onClick={ open_lang() }>
                     <img id="lang-img" className="top-nav-image" alt="Locale Country Flag" title="Language" />
                     {/*{# Translators: End of navigation bar #}*/}
@@ -114,3 +121,5 @@ function Navbar(props) {
         </nav>
     )
 }
+
+export default Navbar;
