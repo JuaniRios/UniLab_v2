6eb244a299d0aaf199f6eab2815e1fdba1130d2d
Juan Ignacio Rios <juani.rios.99@gmail.com>
2021-12-22 13:50:22 +0100

    community fully functional? to test

--- frontend/src/Components/Community/CreatePost.jsx
+++ frontend/src/Components/Community/CreatePost.jsx
@@ -3,15 +3,18 @@ import React, { useState, useEffect } from "react";
 import "./CreatePost.css";
 // IMAGES
 import { NavLink } from "react-router-dom";
+import {useAuthState} from "../../Context";
 
 function CreatePost(props) {
-
+    const {userData} = useAuthState()
     const setPostFormClasses = props.setPostFormClasses;
+    const picStyle = {
+        backgroundImage: `url(${userData.image})`
+    }
 
     return (
         <div className={`create-post flex-row a-i-c w40 shadow`}>
-
-            <NavLink className={`post-pfp shadow`} to="/profile"></NavLink>
+            <NavLink to="/profile" style={picStyle} className={`post-pfp shadow`} />
 
             <h4 className={`action-btn shadow normal`} onClick={setPostFormClasses}>Create a post</h4>
 
