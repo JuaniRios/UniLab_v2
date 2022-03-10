6475e1b8e5b13ad3c1dfd26c1e12b71fa6fbf86d
Juan Ignacio Rios <juani.rios.99@gmail.com>
2022-03-10 13:01:44 +0100

    build for deployment

--- frontend/src/Config/config.js
+++ frontend/src/Config/config.js
@@ -1,6 +1,6 @@
 export const config = {
-  "django_api": "http://unilab.fh-krems.ac.at:8000/api "
+  "django_api": "http://unilab.fh-krems.ac.at:8000/api/ "
 }
 
-// change to http://unilab.fh-krems.ac.at:8000/api for running build
+// change to http://unilab.fh-krems.ac.at:8000/api/ for running build
 // change to http://127.0.0.1:8000/api/ for development
