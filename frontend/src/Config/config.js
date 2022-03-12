ad735d00228d4cbc00e32b2cddc725c32251437f
Juan Ignacio Rios <juani.rios.99@gmail.com>
2022-03-12 16:59:59 +0100

    build available

--- frontend/src/Config/config.js
+++ frontend/src/Config/config.js
@@ -1,7 +1,12 @@
+// require("dotenv").config()
+// NOT WORKING
+
 export const config = {
   // "django_api": "http://unilab.fh-krems.ac.at:8000/api/"
   "django_api": "http://127.0.0.1:8000/api/"
+  // "django_api": process.env["API_URL"]
 }
 
+// console.log('Current directory: ' + process.cwd());
 // change to http://unilab.fh-krems.ac.at:8000/api/ for running build
 // change to http://127.0.0.1:8000/api/ for development
