7a2744f0a21803414e6a9685dca037c73c8b5dcb
Shindovalio <71457593+Shindovalio@users.noreply.github.com>
2022-03-12 16:26:49 +0200

    fix

--- frontend/src/Components/MyCompanies/index.jsx
+++ frontend/src/Components/MyCompanies/index.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import RectangleItem from "../RectangleItem";
 
 import "./index.css";
@@ -6,50 +6,58 @@ import "./index.css";
 import profile from "../../Assets/img/profile.png";
 import NavMenu from "../NavMenu";
 import Footer from "../Footer";
-import {useAuthState} from "../../Context";
+import { useAuthState } from "../../Context";
 import apiCall from "../HelperFunctions/apiCall";
-import {useMessage} from "../../Context/context";
+import { useMessage } from "../../Context/context";
 import urlToPk from "../HelperFunctions/urlToPk";
 import CompanyItem from "./CompanyItem";
 
 export default function MyUniversities(props) {
-	const [message, setMessage] = useMessage()
-	const {token, userData} = useAuthState();
+	const [message, setMessage] = useMessage();
+	const { token, userData } = useAuthState();
 	const [companies, setCompanies] = useState([]);
 
-	useEffect(async ()=>{
+	useEffect(async () => {
 		if (userData) {
 			try {
 				const newCompanies = [];
 
 				// get full data of each company admin relation
 				for (const url of userData.company_admins) {
-					const admin_data = await apiCall(url, token, {method: "GET", fullUrl: true})
+					const admin_data = await apiCall(url, token, { method: "GET", fullUrl: true });
 
 					// get data of each company
-					newCompanies.push(await apiCall(admin_data.company, token, {method: "GET", fullUrl: true}))
+					newCompanies.push(
+						await apiCall(admin_data.company, token, { method: "GET", fullUrl: true })
+					);
 				}
 
 				// get full data of each company
 				// build widget
 				for (const [i, data] of newCompanies.entries()) {
-					setCompanies(prevState => prevState.concat(
-						<CompanyItem key={i} {...data} url={`/companies/${urlToPk(data.url)}`}/>
-					))
+					setCompanies((prevState) =>
+						prevState.concat(
+							<CompanyItem
+								key={i}
+								{...data}
+								url={`/companies/${urlToPk(data.url)}`}
+							/>
+						)
+					);
 				}
 			} catch (e) {
-				setMessage(`error getting companies: ${e}`)
+				setMessage(`error getting companies: ${e}`);
 			}
-
 		}
-
-	},[userData])
+	}, [userData]);
 
 	return (
 		<>
-			<NavMenu/>
-			{companies}
-			<Footer/>
+			<NavMenu />
+			<div className="w60" style={{ margin: "auto" }}>
+				<h1 className="normal">My Companies</h1>
+				{companies}
+			</div>
 		</>
 	);
 }
--- frontend/src/Components/RectangleItem.jsx
+++ frontend/src/Components/RectangleItem.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
 import "./RectangleItem.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function RectangleItem(props) {
 	return (
 		<>
 			{props["url"] ? (
-				<Link className="rect-item-container shadow w100" to={props.url}>
-					{props.img && <img className="rect-item-img" src={props.img}  alt="Company Image"/>}
+				<Link className="rect-item-container shadow" to={props.url} style={props.style}>
+					{props.img && (
+						<img className="rect-item-img" src={props.img} alt="Company Image" />
+					)}
 					<div className="rect-item-content">{props.content}</div>
 					<div className="rect-item-btn-holder">
 						{props.btns &&
@@ -24,8 +26,10 @@ export default function RectangleItem(props) {
 					</div>
 				</Link>
 			) : (
-				<div className="rect-item-container shadow">
-					{props.img && <img className="rect-item-img" src={props.img}  alt="Company Image"/>}
+				<div className="rect-item-container shadow" style={props.style}>
+					{props.img && (
+						<img className="rect-item-img" src={props.img} alt="Company Image" />
+					)}
 					<div className="rect-item-content">{props.content}</div>
 					<div className="rect-item-btn-holder">
 						{props.btns &&
