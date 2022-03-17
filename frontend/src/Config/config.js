22bbee66d6aa8baf2ce664328a80dd7eb1b9168d
Juan Ignacio Rios <juani.rios.99@gmail.com>
2022-03-17 11:24:23 +0100

    normal build with port 8000

--- frontend/src/Config/config.js
+++ frontend/src/Config/config.js
@@ -2,7 +2,7 @@
 // NOT WORKING
 
 export const config = {
-  "django_api": "http://unilab.fh-krems.ac.at:53365/api/"
+  "django_api": "http://unilab.fh-krems.ac.at:8000/api/"
   // "django_api": "http://127.0.0.1:8000/api/"
   // "django_api": process.env["API_URL"]
 }
