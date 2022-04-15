ba1fe5d633e42c0f2947bf657a285d6312190fa8
Juan Ignacio Rios <juani.rios.99@gmail.com>
2022-04-15 17:00:12 +0200

    cors change

--- backend/UniLab/production_settings.py
+++ backend/UniLab/production_settings.py
@@ -3,7 +3,7 @@ from .general_settings import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://unilab.fh-krems.ac.at", "104.214.223.89"]
+ALLOWED_HOSTS = ["unilab.fh-krems.ac.at", "104.214.223.89"]
 PORT = '8000'
 API_URL = "https://unilab.fh-krems.ac.at"
 WEBSITE_URL = "https://unilab.fh-krems.ac.at"
