05fdd9ae7d86fb2a75d305ec6230ba88f69d7286
juani <juani.rios.99@gmail.com>
2022-11-08 23:30:39 +0100

    home buttons broken links fixed

--- frontend/src/Components/Home/HomeBlock.jsx
+++ frontend/src/Components/Home/HomeBlock.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 // STYLES
 import "./HomeBlock.css";
+import {Link} from "react-router-dom";
 
 export default function HomeBlock(props) {
 	const headerValue = props.headerValue;
 	const btnValue = props.btnValue;
+	let href;
+	if (btnValue === "More Companies") {
+		href = "/companies"
+	} else if (btnValue === "Create Account") {
+		href = "/sign-up"
+	} else {
+		href = "/"
+	}
+
 	return (
 		<div className={`home-block shadow`}>
 			<h1 className={`home-block-header`}>{headerValue}</h1>
 			{props.children}
-			<a href="{% url 'signup' %}" className={`home-block-btn uni-button`}>
+			<Link to={href} className={`home-block-btn uni-button`}>
 				{btnValue}
-			</a>
+			</Link>
 		</div>
 	);
 }
